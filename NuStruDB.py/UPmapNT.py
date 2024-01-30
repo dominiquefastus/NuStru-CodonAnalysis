26d9f2ff059b11f4be3e0ea56b65d1e53677fc5c
Dominique Fastus <dom.fast.df@gmail.com>
2024-01-30 17:15:09 +0100

    Update on Uniprot mapping

--- NuStruDB.py/UPmapNT.py
+++ NuStruDB.py/UPmapNT.py
@@ -1,9 +1,6 @@
+import argparse
 import requests
 import json
-
-import asyncio
-
-from multiprocessing import Pool
 
 import mysql.connector
 from mysql.connector import Error
@@ -12,12 +9,16 @@ import sqlite3
 from sqlite3 import Error
 
 from Bio import Entrez
+from Bio import SeqIO
 
+"""
 nustruDB = mysql.connector.connect(
     host="localhost",
     user="root",
+    passwd="AL-16-C988",
     database="nustruDB"
 )
+"""
 
 def create_connection(db_file):
     """
@@ -76,44 +77,103 @@ def execute_database(method, db_file, table, entry_id, sec_id, protein_sequence,
     sqliteConnection.commit() 
     sqliteConnection.close()
     
-def retrieve_nucleotide_seq(ncbiDB="nuccore", genomeID=None, rettype="fasta", retmode="text"):
+def retrieve_nucleotide_seq(ncbiDB="nuccore", entryID=None, rettype="fasta", retmode="text"):
     Entrez.email = "dominique.fastus@biochemistry.lu.se"    
-    handle = Entrez.efetch(db=ncbiDB, id=genomeID, rettype=rettype, retmode=retmode)
+    handle = Entrez.efetch(db=ncbiDB, id=entryID, rettype=rettype, retmode=retmode)
     
     return handle.read()
 
 
 #------------------------------------------------
 
-base_data = 'https://rest.uniprot.org/uniprotkb/search?query=Q9UJV3&fields=gene_primary,sequence,organism_name,cc_subcellular_location'
+
 pdbs_ref = 'https://rest.uniprot.org/uniprotkb/search?query=Q9UJV3&fields=xref_pdb'
-url_genomic_locations = 'https://rest.uniprot.org/uniprotkb/search?query=Q9UJV3&fields=xref_embl' 
 
+def get_base_data(uniprotID):
+    base_data = 'https://rest.uniprot.org/uniprotkb/search?query=%s&fields=gene_primary,sequence,organism_name,cc_subcellular_location' %uniprotID
+    
+    response = requests.get(base_data)
+        
+    if response.status_code == 200:
+        print(response.text)
+        
+        response = json.loads(response.text)
+        
+        organism = response['results'][0]['organism']['scientificName']
+        gene_name = response['results'][0]['genes'][0]['geneName']['value']
+        
+        sequence = response['results'][0]['sequence']['value']
+        print(organism,gene_name,sequence)
+    
+def get_cds(uniprotID):
+    url_ccds = 'https://rest.uniprot.org/uniprotkb/search?query=%s&fields=xref_ccds' %uniprotID
+    url_gene = 'https://rest.uniprot.org/uniprotkb/search?query=%s&fields=xref_embl' %uniprotID
 
-base_data_response = requests.get(base_data)
-if base_data_response.status_code == 200:
-    print(base_data_response.text)
+    response_ccds = requests.get(url_ccds)
+      
+    if response_ccds.status_code == 200:
+        response = json.loads(response_ccds.text)
+        
+        if response['results'][0]['uniProtKBCrossReferences']:
+            for ccds_ref in response['results'][0]['uniProtKBCrossReferences']:
+                ccds_id = ccds_ref['id']
+                try:
+                    isoformId = ccds_ref['isoformId']
+                    print(ccds_id, isoformId)
+                except:
+                    print(ccds_id)
+                
+        else:
+            response_gene = requests.get(url_gene)
+        
+            if response_gene.status_code == 200: 
+                cds_ids = []
+                protein_ids = []
+                
+                response = json.loads(response_gene.text)
+                print(response)
+                
+                if response['results'][0]['uniProtKBCrossReferences']:
+                    for cds_ref in response['results'][0]['uniProtKBCrossReferences']:
+                        cds_id = cds_ref['id']
+                        
+                        for properties in cds_ref['properties']:
 
+                            if properties['key'] == 'ProteinId':
+                                protein_id = properties['value']
+                        
+                            if properties['key'] == 'Status':
+                                status_code = properties['value']
+                            
+                        if status_code == "-":
+                            cds_ids.append(cds_id)
+                            protein_ids.append(protein_id)
+                        
+        return cds_ids, protein_ids
+    
+    
+def main():
+    parser = argparse.ArgumentParser(
+        prog="PDBmapNT",
+        description="Maps Uniprot ID to nucleotide sequence and prints an allignment of the pdb protein sequence to the nucleotide sequence"
+    )
+    parser.add_argument('entryID')
+    args = parser.parse_args()
+    
+    create_connection("nustru.db")
+    
+    with open(args.entryID,'r') as entryIDs_file:
+        entryIDs_file = entryIDs_file.read()
 
-response = requests.get(url_genomic_locations)
-ref_available = False
+        for uniprot_id in entryIDs_file.split(','):
+            
+            
+            handle = retrieve_nucleotide_seq(ncbiDB="nuccore",entryID=ids[4], rettype="fasta_cds_na")
+            print(handle)
     
-if response.status_code == 200:
-    print(response.text)
+if __name__ == '__main__':
+    get_base_data("P10486")
+    cds_ids, protein_ids = get_cds("P02185")
+
     
-    ids = []
-    response = json.loads(response.text)
-    if response['results'][0]['uniProtKBCrossReferences']:
-        ref_available = True
-        for ccds_ref in response['results'][0]['uniProtKBCrossReferences']:
-            ccds_id = ccds_ref['id']
-            ids.append(ccds_id)
-            try:
-                isoformId = ccds_ref['isoformId']
-                print(ccds_id, isoformId)
-            except:
-                print(ccds_id)
-    
-    
-    handle = retrieve_nucleotide_seq(ncbiDB="nuccore",id=ids[0], rettype="fasta_cds_na")
-    print(handle)
+    print(retrieve_nucleotide_seq(entryID=cds_ids, rettype="fasta_cds_na"))
