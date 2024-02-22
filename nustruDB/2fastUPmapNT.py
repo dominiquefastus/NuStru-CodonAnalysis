d4c61927bd7af037a8928d93d535d8d1a38e8d65
Dominique Fastus <dom.fast.df@gmail.com>
2024-02-23 00:03:09 +0100

    Faster UP mapping implementation

---
+++ nustruDB/2fastUPmapNT.py
@@ -0,0 +1,76 @@
+import pandas as pd 
+import numpy as np
+pd.options.mode.chained_assignment = None
+pd.set_option('future.no_silent_downcasting', True)
+
+from Bio import Entrez
+from Bio import SeqIO
+
+from typing import List
+
+from pandarallel import pandarallel
+
+pandarallel.initialize(progress_bar=True)
+
+overall_cds = []
+overall_protein_ids = []
+
+
+def retrieve_nucleotide_seq(entryID=None, protein_id=None):
+    Entrez.email = "dominique.fastus@biochemistry.lu.se"    
+    
+    with Entrez.efetch(db="nuccore", rettype="fasta_cds_na", retmode="text", id=entryID) as handle:
+        for seq_record in SeqIO.parse(handle, "fasta"):
+            if protein_id in seq_record.description:
+                head = seq_record.id + seq_record.description
+                sequence = f'{entryID}:{seq_record.seq}'
+
+    return head, sequence
+
+def retrieve_protein_seq(entryID=None):
+    Entrez.email = "dominique.fastus@biochemistry.lu.se"    
+    
+    with Entrez.efetch(db="protein", rettype="fasta", retmode="text", id=entryID) as handle:
+        for seq_record in SeqIO.parse(handle, "fasta"):
+            sequence = seq_record.seq
+    return sequence
+
+def get_cds(data):
+    id = str(data['nucleotide_id']).strip().replace('"', '').replace(' ', '')
+    id_list = id.split(';')
+    del id_list[-1]
+    
+    cds_ids = id_list[0::4]
+    protein_ids = id_list[1::4]
+    status = id_list[2::4]
+    
+    try:
+        for cds_id, protein_id, status in zip(cds_ids, protein_ids, status):
+            if status == '-':            
+                if retrieve_protein_seq(protein_id) == data['protein_sequence']:
+                    
+                    matched_id, matched_seq = retrieve_nucleotide_seq(cds_id, protein_id)
+                    break  
+                        
+        with open('/Users/dominiquefastus/Downloads/sequences.fasta', 'a') as f:
+            f.write(f'>{matched_id}\n{matched_seq}\n')
+    except:
+        matched_seq = "NaN"
+
+    return matched_seq
+
+def main():
+    
+    df = pd.read_csv('/Users/dominiquefastus/Downloads/uniprotkb_taxonomy_id_562_2024_02_19.tsv', sep='\t', header=0, nrows=400, dtype={"Subcellular location [CC]": object,
+                                                                                                                         "Alternative sequence": object})
+    df = df.replace(r'^\s*$', np.nan, regex=True)
+
+    nucleotide_protein_seqs_df = df[['Entry', 'Gene Names (primary)', 'Organism', 'Subcellular location [CC]', 'Sequence', 'EMBL']]
+    nucleotide_protein_seqs_df.columns = ['primary_id', 'gene_name', 'organism', 'expression_system', 'protein_sequence', 'nucleotide_id']
+    
+    nucleotide_protein_seqs_df['nucleotide_sequence'] = nucleotide_protein_seqs_df.parallel_apply(get_cds, axis=1)
+    nucleotide_protein_seqs_df[['nucleotide_id','nucleotide_sequence']] = nucleotide_protein_seqs_df['nucleotide_sequence'].str.split(':',expand=True)
+    nucleotide_protein_seqs_df.to_csv('/Users/dominiquefastus/Downloads/nucleotide_protein_seqs.csv', index=False)
+    
+if __name__ == '__main__':
+    main()
