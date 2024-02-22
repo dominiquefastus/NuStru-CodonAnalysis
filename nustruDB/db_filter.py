e7359e0d1ec88da8cbff1b6d86c1104a929627aa
Dominique Fastus <dom.fast.df@gmail.com>
2024-02-22 23:58:36 +0100

    Added database translation filter

--- nustruDB/db_filter.py
+++ nustruDB/db_filter.py
@@ -12,20 +12,39 @@ df = pd.read_csv('/Users/dominiquefastus/master_project/NuStru/nustruDB/E_COLI_K
 
 df_reduced = df.drop_duplicates(subset=['primary_id','nucleotide_sequence'])
 
+df_reduced.to_csv('/Users/dominiquefastus/master_project/NuStru/nustruDB/E_COLI_K12_02_reduced.csv', index=False)
+
 def nucleotide_to_protein(data, codons = 3):
     translated_sequence = data['nucleotide_sequence'].str[:codons].apply(lambda x: Seq(x).translate())
     
     return translated_sequence
 
 
-def check_first_codon(data):
-    first_base_tranlsate = nucleotide_to_protein(data=data, codons=3)
+def correct_first_codon(data):
+    base_tranlsate = nucleotide_to_protein(data=data, codons=3)
+    
 
-    if data['protein_sequence'][0] != first_base_tranlsate:
-        print(data['protein_sequence'][0])
+    print(base_tranlsate)
+    
+    mask_df_reduced = (data['protein_sequence'].str[0:7] != base_tranlsate)
+    
+    base_difference = data[mask_df_reduced]
+    
+    print(base_difference)
 
+
+    '''
+    if data['protein_sequence'][0] != first_base_tranlsate[0]:
+        data['nucleotide_sequence'].str[0] = "A"
+        '''
+    
+        
 def main():
-    check_first_codon(df_reduced)
+    # df_reduced = df_reduced.apply(correct_first_codon, axis=1)
+    
+    # print(df_reduced)
+
+    correct_first_codon(df_reduced)
 
 if __name__ == '__main__':
-    print('test')
+    main()
