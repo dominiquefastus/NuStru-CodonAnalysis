9a976816d963b3f78fa8188bba5e54329c5ec2be
Dominique Fastus <dom.fast.df@gmail.com>
2024-03-07 23:31:43 +0100

    Bug fixes and added concurrent apply

--- nustruDB/db_fetch.py
+++ nustruDB/db_fetch.py
@@ -9,10 +9,13 @@ from biopandas.pdb import PandasPdb
 from Bio.PDB import PDBParser
 from Bio.PDB.DSSP import DSSP
 
-df = pd.read_csv('/Users/dominiquefastus/master_project/NuStru/nustruDB/E_COLI_K12_02_reduced.csv', nrows=10, index_col=False)
+from pandarallel import pandarallel
+pandarallel.initialize(progress_bar=True)
+
+nucleotide_protein_seqs_df = pd.read_csv('/Users/dominiquefastus/master_project/NuStru/nustruDB/E_COLI_K12_02_reduced.csv', nrows=10, index_col=False)
 
 def fetch_pdb_and_plddt(data):
-    p = PDBParser()
+    p = PDBParser(QUIET=True)
     if data['source'] == 'uniprot':
         ppdb = PandasPdb().fetch_pdb(uniprot_id=data['primary_id'], source="alphafold2-v2")
         residue_plddt = ppdb.df["ATOM"][["b_factor","residue_number"]]
@@ -32,13 +35,13 @@ def fetch_pdb_and_plddt(data):
         residue_b_factor_dict = residue_b_factor.to_dict()
         
         data['plddt'] = residue_b_factor_dict
-    
 
     new_data = pd.DataFrame({'source': data['source'], 'primary_id': data['primary_id'], 'gene_name': data['gene_name'], 'organism': data['organism'], 'expression_system': data['expression_system'],
-                            'protein_sequence': data['protein_sequence'], 'nucleotide_id': data['nucleotide_id'], 'nucleotide_sequence': data['nucleotide_sequence'], 'plddt': data['plddt']}, index=[0])
+                            'protein_sequence': data['protein_sequence'], 'nucleotide_id': data['nucleotide_id'], 'nucleotide_sequence': data['nucleotide_sequence'], 'plddt': data['plddt']})
     new_data.to_csv('/Users/dominiquefastus/Downloads/structure_info.csv', mode='a', index=False, header=False)
     
     ppdb.to_pdb(path='/Users/dominiquefastus/Downloads/{}.pdb'.format(data['primary_id']))
+
     
     structure = p.get_structure(data["primary_id"], "/Users/dominiquefastus/Downloads/{}.pdb".format(data['primary_id']))
     model = structure[0]
@@ -49,4 +52,4 @@ def fetch_pdb_and_plddt(data):
 
 with open('/Users/dominiquefastus/Downloads/structure_info.csv', mode='w') as f:
     f.write('source,primary_id,gene_name,organism,expression_system,protein_sequence,nucleotide_id,nucleotide_sequence,plddt,secondary_structure\n')
-df.apply(fetch_pdb_and_plddt, axis=1)
+nucleotide_protein_seqs_df.parallel_apply(fetch_pdb_and_plddt, axis=1)
