4bbe47b3abe8c0c1fd9dee61a241d538827b0865
Dominique Fastus <dom.fast.df@gmail.com>
2024-03-10 17:10:40 +0100

    Added command line arguments

--- nustruDB/db_fetch.py
+++ nustruDB/db_fetch.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import argparse
 import pandas as pd
 import numpy as np
 
@@ -12,12 +13,11 @@ from Bio.PDB.DSSP import DSSP
 from pandarallel import pandarallel
 pandarallel.initialize(progress_bar=True)
 
-nucleotide_protein_seqs_df = pd.read_csv('/Users/dominiquefastus/master_project/NuStru/nustruDB/E_COLI_K12_02_reduced.csv', nrows=10, index_col=False)
-
-def fetch_pdb_and_plddt(data):
+def fetch_pdb_and_plddt(data, output_path, name):
     p = PDBParser(QUIET=True)
     if data['source'] == 'uniprot':
-        ppdb = PandasPdb().fetch_pdb(uniprot_id=data['primary_id'], source="alphafold2-v2")
+        print(data['primary_id'])
+        ppdb = PandasPdb().fetch_pdb(uniprot_id=data['primary_id'], source="alphafold2-v4")
         residue_plddt = ppdb.df["ATOM"][["b_factor","residue_number"]]
 
 
@@ -27,6 +27,7 @@ def fetch_pdb_and_plddt(data):
         data['bfactor_or_plddt'] = residue_plddt_dict
         
     else:
+        print(data['primary_id'])
         ppdb = PandasPdb().fetch_pdb(pdb_code=data['primary_id'], source="pdb")
         residue_b_factor = ppdb.df["ATOM"][["b_factor","residue_number"]]
         
@@ -36,21 +37,50 @@ def fetch_pdb_and_plddt(data):
         
         data['bfactor_or_plddt'] = residue_b_factor_dict
     
-    ppdb.to_pdb(path='/Users/dominiquefastus/Downloads/{}.pdb'.format(data['primary_id']))
 
-    
-    structure = p.get_structure(data["primary_id"], "/Users/dominiquefastus/Downloads/{}.pdb".format(data['primary_id']))
-    model = structure[0]
-    dssp = DSSP(model, "/Users/dominiquefastus/Downloads/{}.pdb".format(data['primary_id']))
-    secondary_structure = "".join([dssp_data[2] for dssp_data in dssp])
-    data['secondary_structure'] = secondary_structure
+    ppdb.to_pdb(path=f'{output_path}/{data['primary_id']}.pdb')
+
+    try:
+        structure = p.get_structure(data["primary_id"], f"{output_path}/{data['primary_id']}.pdb")
+        model = structure[0]
+        dssp = DSSP(model, f"{output_path}/{data['primary_id']}.pdb")
+        secondary_structure = "".join([dssp_data[2] for dssp_data in dssp])
+        data['secondary_structure'] = secondary_structure
+    except:
+        data['secondary_structure'] = 'NaN'
 
     new_data = pd.DataFrame({'source': data['source'], 'primary_id': data['primary_id'], 'gene_name': data['gene_name'], 'organism': data['organism'], 'expression_system': data['expression_system'],
                             'protein_sequence': data['protein_sequence'], 'nucleotide_id': data['nucleotide_id'], 'nucleotide_sequence': data['nucleotide_sequence'], 'bfactor_or_plddt': data['bfactor_or_plddt'], 'secondary_structure': data['secondary_structure']})
-    new_data.to_csv('/Users/dominiquefastus/Downloads/structure_info.csv', mode='a', index=False, header=False)
+    new_data.to_csv(f'{output_path}/{name}.csv', mode='a', index=False, header=False)
+    del new_data
 
-
-with open('/Users/dominiquefastus/Downloads/structure_info.csv', mode='w') as f:
-    f.write('source,primary_id,gene_name,organism,expression_system,protein_sequence,nucleotide_id,nucleotide_sequence,bfactor_or_plddt,secondary_structure\n')
+def main():
     
-nucleotide_protein_seqs_df.apply(fetch_pdb_and_plddt, axis=1)
+    parser = argparse.ArgumentParser(
+        prog='db_fetch.py',
+        description="Fetch the pdb and map the plddt or bfactor to the protein sequence."
+    )
+    parser.add_argument(
+        '-i', '--input', type=str, dest="input_file", required=True,
+        help='Input file of csv formatted uniprot or pdb entries.'
+    )
+    parser.add_argument( 
+        '-o', '--output', type=str, dest="output_path", required=True,
+        help='Output to store the new csv with secondary structure information.'
+    )
+    parser.add_argument(
+        '-n', '--name', type=str, dest="name", required=True,
+        help='Name of the output files and log file.'
+    )
+
+    args = parser.parse_args()
+    
+    nucleotide_protein_seqs_df = pd.read_csv(args.input_file, index_col=False)
+    
+    with open(f'{args.output_path}/{args.name}.csv', mode='w') as f:
+        f.write('source,primary_id,gene_name,organism,expression_system,protein_sequence,nucleotide_id,nucleotide_sequence,bfactor_or_plddt,secondary_structure\n')
+        
+    nucleotide_protein_seqs_df.apply(lambda data: fetch_pdb_and_plddt(data=data, output_path=args.output_path, name=args.name), axis=1)
+    
+if __name__ == "__main__":
+    main()
