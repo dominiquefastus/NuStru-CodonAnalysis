26b33cc4d874c31a3daf3a957f70a2e929038897
Dominique Fastus <dom.fast.df@gmail.com>
2024-03-19 16:48:00 +0100

    Changed to cif files, added error handling and logging

--- nustruDB/db_fetch.py
+++ nustruDB/db_fetch.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import logging
 import pandas as pd
 import numpy as np
 
@@ -12,7 +13,6 @@ from urllib.error import HTTPError
 
 from biopandas.pdb import PandasPdb 
 
-from Bio.PDB import PDBParser
 from Bio.PDB import MMCIFParser
 from Bio.PDB.DSSP import DSSP
 
@@ -20,55 +20,67 @@ from pandarallel import pandarallel
 pandarallel.initialize(progress_bar=True)
 
 def fetch_pdb_and_plddt(data, output_path, name):
-    pdbp = PDBParser(QUIET=True)
     cifp = MMCIFParser(QUIET=True)
     
     Path(f'{output_path}/pdb_files').mkdir(parents=True, exist_ok=True)
     Path(f'{output_path}/cif_files').mkdir(parents=True, exist_ok=True)
     
-    if data['source'] == 'uniprot':
-        print(data['primary_id'])
-        ppdb = PandasPdb().fetch_pdb(uniprot_id=data['primary_id'], source="alphafold2-v4")
-        
-        af2_version = 4
-        urlretrieve(f"https://alphafold.ebi.ac.uk/files/AF-{data['primary_id']}-F1-model_v{af2_version}.cif", f"{output_path}/cif_files/{data['primary_id']}.cif")
-        
-        residue_plddt = ppdb.df["ATOM"][["b_factor","residue_number"]]
+    try:
+        if data['source'] == 'uniprot':
+            model_id = data['primary_id']
+            ppdb = PandasPdb().fetch_pdb(uniprot_id=model_id, source="alphafold2-v4")
+            
+            try:
+                af2_version = 4
+                urlretrieve(f"https://alphafold.ebi.ac.uk/files/AF-{model_id}-F1-model_v{af2_version}.cif", f"{output_path}/cif_files/{model_id}.cif")
+            except HTTPError as e:
+                logging.error(f"Error: could not retrieve cif file for {model_id}.")
+                logging.error(e)
+            
+            residue_plddt = ppdb.df["ATOM"][["b_factor","residue_number"]]
 
 
-        residue_plddt = residue_plddt.groupby(["residue_number"]).mean().round(4)
+            residue_plddt = residue_plddt.groupby(["residue_number"]).mean().round(4)
 
-        residue_plddt_dict = residue_plddt.to_dict()
-        data['bfactor_or_plddt'] = residue_plddt_dict
-        
-    else:
-        print(data['primary_id'])
-        ppdb = PandasPdb().fetch_pdb(pdb_code=data['primary_id'], source="pdb")
-        
-        Path(f'{output_path}/pdb_files').mkdir(parents=True, exist_ok=True)
-        urlretrieve(f"https://files.rcsb.org/download/{data["primary_id"]}.cif", f"{output_path}/cif_files/{data['primary_id']}.cif")
-        
-        residue_b_factor = ppdb.df["ATOM"][["b_factor","residue_number"]]
-        
-        residue_b_factor = residue_b_factor.groupby(["residue_number"]).mean().round(4)
+            residue_plddt_dict = residue_plddt.to_dict()
+            data['bfactor_or_plddt'] = residue_plddt_dict
+            
+        else:
+            model_id = data['primary_id'].replace('"','').split("_")[0]
+            ppdb = PandasPdb().fetch_pdb(pdb_code=model_id, source="pdb")
+            
+            Path(f'{output_path}/pdb_files').mkdir(parents=True, exist_ok=True)
+            
+            try:
+                urlretrieve(f"https://files.rcsb.org/download/{model_id}.cif", f"{output_path}/cif_files/{model_id}.cif")
+            except HTTPError as e:
+                logging.error(f"Error: could not retrieve cif file for {model_id}.")
+                logging.error(e)
+            
+            residue_b_factor = ppdb.df["ATOM"][["b_factor","residue_number"]]
+            
+            residue_b_factor = residue_b_factor.groupby(["residue_number"]).mean().round(4)
 
-        residue_b_factor_dict = residue_b_factor.to_dict()
+            residue_b_factor_dict = residue_b_factor.to_dict()
+            
+            data['bfactor_or_plddt'] = residue_b_factor_dict
         
-        data['bfactor_or_plddt'] = residue_b_factor_dict
-    
 
-    ppdb.to_pdb(path=f'{output_path}/pdb_files/{data['primary_id']}.pdb')
+        ppdb.to_pdb(path=f'{output_path}/pdb_files/{model_id}.pdb')
 
-    structure = cifp.get_structure(data["primary_id"], f"{output_path}/cif_files/{data['primary_id']}.cif")
-    model = structure[0]
-    dssp = DSSP(model, f"{output_path}/{data['primary_id']}.cif")
-    secondary_structure = "".join([dssp_data[2] for dssp_data in dssp])
-    data['secondary_structure'] = secondary_structure
+        structure = cifp.get_structure(model_id, f"{output_path}/cif_files/{model_id}.cif")
+        model = structure[0]
+        dssp = DSSP(model, f"{output_path}/cif_files/{model_id}.cif")
+        secondary_structure = "".join([dssp_data[2] for dssp_data in dssp])
+        data['secondary_structure'] = secondary_structure
 
-    new_data = pd.DataFrame({'source': data['source'], 'primary_id': data['primary_id'], 'gene_name': data['gene_name'], 'organism': data['organism'], 'expression_system': data['expression_system'],
-                            'protein_sequence': data['protein_sequence'], 'nucleotide_id': data['nucleotide_id'], 'nucleotide_sequence': data['nucleotide_sequence'], 'bfactor_or_plddt': data['bfactor_or_plddt'], 'secondary_structure': data['secondary_structure']})
-    new_data.to_csv(f'{output_path}/{name}.csv', mode='a', index=False, header=False)
-    del new_data
+        new_data = pd.DataFrame({'source': data['source'], 'primary_id': data['primary_id'], 'gene_name': data['gene_name'], 'organism': data['organism'], 'expression_system': data['expression_system'],
+                                'protein_sequence': data['protein_sequence'], 'nucleotide_id': data['nucleotide_id'], 'nucleotide_sequence': data['nucleotide_sequence'], 'bfactor_or_plddt': data['bfactor_or_plddt'], 'secondary_structure': data['secondary_structure']})
+        new_data.to_csv(f'{output_path}/{name}.csv', mode='a', index=False, header=False)
+        del new_data
+    except:
+        logging.error(f"Error: {data['primary_id']} not found.")
+        pass
 
 def main():
     
@@ -88,8 +100,13 @@ def main():
         '-n', '--name', type=str, dest="name", required=True,
         help='Name of the output files and log file.'
     )
-
     args = parser.parse_args()
+    
+    logging.basicConfig(filename=f'{args.output_path}/{args.name}.log',
+            filemode='a',
+            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+            datefmt='%H:%M:%S',
+            level=logging.ERROR)
     
     nucleotide_protein_seqs_df = pd.read_csv(args.input_file, index_col=False)
     
