9cdcef515db8b936d923a2b49c0d0b25a7ed59da
Dominique Fastus <dom.fast.df@gmail.com>
2024-05-16 18:52:37 +0200

    Update to fetch protein family and uniprot data

--- nustruDB/fetchINPRO.py
+++ nustruDB/fetchINPRO.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import asyncio
 import argparse
 from aiohttp import ClientSession
---
+++ nustruDB/fetchUPTSV.py
@@ -0,0 +1,170 @@
+import re
+import time
+import json
+import zlib
+import requests
+import argparse
+import pandas as pd
+from xml.etree import ElementTree
+from urllib.parse import urlparse, parse_qs, urlencode
+from requests.adapters import HTTPAdapter, Retry
+
+POLLING_INTERVAL = 3
+API_URL = "https://rest.uniprot.org"
+
+retries = Retry(total=5, backoff_factor=0.25, status_forcelist=[500, 502, 503, 504])
+session = requests.Session()
+session.mount("https://", HTTPAdapter(max_retries=retries))
+
+def check_response(response):
+    try:
+        response.raise_for_status()
+    except requests.HTTPError:
+        print(response.json())
+        raise
+
+def submit_id_mapping(from_db, to_db, ids):
+    request = requests.post(
+        f"{API_URL}/idmapping/run",
+        data={"from": from_db, "to": to_db, "ids": ",".join(ids)},
+    )
+    check_response(request)
+    return request.json()["jobId"]
+
+def get_next_link(headers):
+    re_next_link = re.compile(r'<(.+)>; rel="next"')
+    if "Link" in headers:
+        match = re_next_link.match(headers["Link"])
+        if match:
+            return match.group(1)
+
+def check_id_mapping_results_ready(job_id):
+    while True:
+        request = session.get(f"{API_URL}/idmapping/status/{job_id}")
+        check_response(request)
+        j = request.json()
+        if "jobStatus" in j:
+            if j["jobStatus"] == "RUNNING":
+                print(f"Retrying in {POLLING_INTERVAL}s")
+                time.sleep(POLLING_INTERVAL)
+            else:
+                raise Exception(j["jobStatus"])
+        else:
+            return bool(j["results"] or j["failedIds"])
+
+def get_batch(batch_response, file_format, compressed):
+    batch_url = get_next_link(batch_response.headers)
+    while batch_url:
+        batch_response = session.get(batch_url)
+        batch_response.raise_for_status()
+        yield decode_results(batch_response, file_format, compressed)
+        batch_url = get_next_link(batch_response.headers)
+
+def combine_batches(all_results, batch_results, file_format):
+    if file_format == "json":
+        for key in ("results", "failedIds"):
+            if key in batch_results and batch_results[key]:
+                all_results[key] += batch_results[key]
+    elif file_format == "tsv":
+        return all_results + batch_results[1:]
+    else:
+        return all_results + batch_results
+    return all_results
+
+def get_id_mapping_results_link(job_id):
+    url = f"{API_URL}/idmapping/details/{job_id}"
+    request = session.get(url)
+    check_response(request)
+    return request.json()["redirectURL"]
+
+def decode_results(response, file_format, compressed):
+    if compressed:
+        decompressed = zlib.decompress(response.content, 16 + zlib.MAX_WBITS)
+        if file_format == "json":
+            j = json.loads(decompressed.decode("utf-8"))
+            return j
+        elif file_format == "tsv":
+            return [line for line in decompressed.decode("utf-8").split("\n") if line]
+        elif file_format == "xlsx":
+            return [decompressed]
+        elif file_format == "xml":
+            return [decompressed.decode("utf-8")]
+        else:
+            return decompressed.decode("utf-8")
+    elif file_format == "json":
+        return response.json()
+    elif file_format == "tsv":
+        return [line for line in response.text.split("\n") if line]
+    elif file_format == "xlsx":
+        return [response.content]
+    elif file_format == "xml":
+        return [response.text]
+    return response.text
+
+def print_progress_batches(batch_index, size, total):
+    n_fetched = min((batch_index + 1) * size, total)
+    print(f"Fetched: {n_fetched} / {total}")
+
+def get_id_mapping_results_search(url):
+    print(url)
+    parsed = urlparse(url)
+    query = parse_qs(parsed.query)
+    file_format = query["format"][0] if "format" in query else "json"
+    if "size" in query:
+        size = int(query["size"][0])
+    else:
+        size = 500
+        query["size"] = size
+    compressed = (
+        query["compressed"][0].lower() == "true" if "compressed" in query else False
+    )
+    
+    parsed = parsed._replace(query=urlencode(query, doseq=True))
+    url = parsed.geturl()
+    request = session.get(url)
+    check_response(request)
+    results = decode_results(request, file_format, compressed)
+    total = int(request.headers["x-total-results"])
+    print_progress_batches(0, size, total)
+    for i, batch in enumerate(get_batch(request, file_format, compressed), 1):
+        results = combine_batches(results, batch, file_format)
+        print_progress_batches(i, size, total)
+    return results
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        prog='2fastUPmapNT.py',
+        description="Retrieve nucleotide sequences from uniprot IDs."
+    )
+    parser.add_argument(
+        '-i', '--input', type=str, dest="input_file", required=True,
+        help='Input file with uniprot IDs.'
+    )
+    parser.add_argument( 
+        '-o', '--output', type=str, dest="output_path", required=True,
+        help='Output path or directory to store the data.'
+    )
+    parser.add_argument(
+        '-n', '--name', type=str, dest="name", required=True,
+        help='Name of the output file.'
+    )
+    
+    args = parser.parse_args()
+    
+    with open(args.input_file, "r") as f:
+        ids = f.read().splitlines()
+        
+    job_id = submit_id_mapping(from_db="UniProtKB_AC-ID", to_db="UniProtKB", ids=ids)
+    
+    if check_id_mapping_results_ready(job_id):
+        link = get_id_mapping_results_link(job_id)
+        results = get_id_mapping_results_search(link + "?format=tsv" + "&fields=accession%2Cgene_primary%2Corganism_name%2Ccc_subcellular_location%2Csequence%2Cxref_embl_full%2Cxref_ccds%2Cxref_refseq_full%2Ccc_alternative_products%2Cft_var_seq") 
+
+    columns = results[0].split("\t")
+    data = [line.split("\t") for line in results[1:]]
+    df = pd.DataFrame(data, columns=columns)
+    df.drop(columns=["From"], inplace=True)
+    df.to_csv(f"{args.output_path}/{args.name}.tsv", sep="\t", index=False)
+    
+if __name__ == "__main__":
+    main()
